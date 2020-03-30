b7bad7b7de1dc72aca72474abcf8b53a65942437
ikhidebright <ikhidebright@gmail.com>
2020-03-30 11:06:25 +0100

    added pend files

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Notepad from '../views/Notepad.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/pens',
+    name: 'Notepad',
+    component: Notepad
+  },
+  {
+    path: '/pens',
+    name: 'Pens',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Pens.vue')
   }
 ]
 
